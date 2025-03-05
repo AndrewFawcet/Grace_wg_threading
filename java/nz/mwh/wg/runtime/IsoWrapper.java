4c8e157a39db5b8c21b2bcfe48f20917e0f7a850
AndrewFawcet <andrewfawcet@gmail.com>
2025-03-05 17:32:33 +1300

    public BaseObject methods copied into isowrapper for forwarding

--- java/nz/mwh/wg/runtime/IsoWrapper.java
+++ java/nz/mwh/wg/runtime/IsoWrapper.java
@@ -10,12 +10,22 @@ import nz.mwh.wg.Evaluator;
 public class IsoWrapper implements GraceObject {
     private BaseObject isoObject;
     private boolean isAlive = true; // Used to track if the wrapper is "killed"
-    
+
     public IsoWrapper(BaseObject isoObject) {
         if (!isoObject.isIsolated()) {
             throw new RuntimeException("IsoWrapper can only wrap isolated objects.");
         }
         this.isoObject = isoObject;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    private void checkAlive() {
+        if (!isAlive || isoObject == null) {
+            throw new RuntimeException("IsoWrapper is dead; cannot forward requests.");
+        }
     }
 
     public synchronized void kill() {
@@ -26,6 +36,7 @@ public class IsoWrapper implements GraceObject {
         this.isAlive = false;
     }
 
+    // TODO apply for later killing of wrappers
     // TODO does this method need to be synchronised?
     public synchronized void replaceIsoObject(BaseObject newIso) {
         if (!isAlive) {
@@ -37,20 +48,128 @@ public class IsoWrapper implements GraceObject {
         this.isoObject = newIso;
     }
 
+    // All that follows is forwarding duplicates of public methods in BaseObject
+    // Forwarding methods to the wrapped isoObject
+
+    public boolean isLocal() {
+        checkAlive();
+        return isoObject.isLocal();
+    }
+    
+    public boolean isIsolated() {
+        checkAlive();
+        return isoObject.isIsolated();
+    }
+    
+    public boolean isImmutable() {
+        checkAlive();
+        return isoObject.isImmutable();
+    }
+    
+    public void setLocal(boolean isLocal) {
+        checkAlive();
+        isoObject.setLocal(isLocal);
+    }
+    
+    public void setIsolated(boolean isIsolated) {
+        checkAlive();
+        isoObject.setIsolated(isIsolated);
+    }
+    
+    public void setImmutable(boolean isImmutable) {
+        checkAlive();
+        isoObject.setImmutable(isImmutable);
+    }
+    
+    public Thread getObjectThread() {
+        checkAlive();
+        return isoObject.getObjectThread();
+    }
+    
+    public int getReferenceCount() {
+        checkAlive();
+        return isoObject.getReferenceCount();
+    }
+    
+    public void incrementReferenceCount() {
+        checkAlive();
+        isoObject.incrementReferenceCount();
+    }
+    
+    public void decrementReferenceCount() {
+        checkAlive();
+        isoObject.decrementReferenceCount();
+    }
+    
+    public void setAliasName(String name) {
+        checkAlive();
+        isoObject.setAliasName(name);
+    }
+    
+    public String getAliasName() {
+        checkAlive();
+        return isoObject.getAliasName();
+    }
+    
+    public void setAliasObject(GraceObject object) {
+        checkAlive();
+        isoObject.setAliasObject(object);
+    }
+    
+    public GraceObject getAliasObject() {
+        checkAlive();
+        return isoObject.getAliasObject();
+    }
+    
+    public boolean isAutoUnlinkingIsoMoves() {
+        checkAlive();
+        return isoObject.isAutoUnlinkingIsoMoves();
+    }
+    
+    public boolean isThreadBoundaryLocalChecking() {
+        checkAlive();
+        return isoObject.isThreadBoundaryLocalChecking();
+    }
+    
+    @Override
+    public String toString() {
+        checkAlive();
+        return isoObject.toString();
+    }
+    
+    public void addMethod(String name, Function<Request, GraceObject> method) {
+        checkAlive();
+        isoObject.addMethod(name, method);
+    }
+    
     @Override
     public GraceObject request(Request request) {
-        if (!isAlive || isoObject == null) {
-            throw new RuntimeException("IsoWrapper is dead; cannot forward requests.");
-        }
+        checkAlive();
         return isoObject.request(request);
     }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
+    
     public GraceObject findReceiver(String name) {
-
+        checkAlive();
         return isoObject.findReceiver(name);
+    }
+    
+    public void addField(String name) {
+        checkAlive();
+        isoObject.addField(name);
+    }
+    
+    public void addFieldWriter(String name) {
+        checkAlive();
+        isoObject.addFieldWriter(name);
+    }
+    
+    public void setField(String name, GraceObject value) {
+        checkAlive();
+        isoObject.setField(name, value);
+    }
+    
+    public GraceObject findReturnContext() {
+        checkAlive();
+        return isoObject.findReturnContext();
     }
 }
